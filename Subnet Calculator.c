36d10177dea1362f919a820df20125851a9d870e
Benjamin Wagrez <benwagrez@gmail.com>
2020-04-21 08:00:16 -0400

    Introducing Subnet Calc

--- Subnet Calculator.c
+++ Subnet Calculator.c
@@ -39,26 +39,37 @@ int main()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
  
- 
+}
+
+int subnetCalculator{ // Not fully implemented -- just conceptual -- Will likely split into multiplefunctions
+	/* OCT variable for each octet x.x.x.x and CIDR for mask */
+	int OCT0, OCT1, OCT2, OCT3, CIDR; 
+	/* Finding IP class */
+	char class;	
+
+	/* Conditional statement to remove invalid IP addresses */
+	if( ((OCT0 < 0) || (OCT1 < 0) || (OCT2 < 0) || (OCT3 < 0)) || 
+	((OCT0 > 255) || (OCT1 > 255) || (OCT2 > 255) || (OCT3 > 255)) || 
+	((OCT0 == 127)) || 
+	((OCT0 == 0) && (OCT1 == 0) && (OCT2 == 0) && (OCT3 == 0)){
+		
+		return -1; // Conceptual; requery user for a correct IP
+	}
+
+	/* Conditional statement to remove invalid CIDR */
+	if(CIDR > 32 || CIDR < 0){
+		return -1; // Conceptual; requery user for a correct CIDR
+	}
+	
+	/* Conditional statement to assign IP class */
+	if(OCT0 <=127){ // Assigning Class
+		class = 'A';
+	}else if (OCT0 <=192){
+		class = 'B';
+	} else{
+		class = 'C';
+	}
+	
+	
 }
